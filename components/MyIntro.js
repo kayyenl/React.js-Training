b17d4fa79f52a902dd27dc620917d47a3252c94b
kayyenl <yenyenlee9@gmail.com>
2022-07-18 23:06:37 +0800

    adding one extra component

---
+++ components/MyIntro.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MyIntro = () => {
+    return (
+        <div>
+            <h1>Lee Kay Yen</h1>
+            <p> I am a student going on exchange to University of Tokyo in 1.5 months time! Below are my top 3 places to go in Tokyo.</p>
+            <ul>
+                <li>Family Mart</li>
+                <li>Akihabara</li>
+                <li>Run-down malls</li>
+            </ul>   
+        </div>
+    );
+}
+
+export default MyIntro;
