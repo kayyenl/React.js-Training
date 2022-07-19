cb63e65a28544d5f7710b5243099808a5e738d44
kayyenl <yenyenlee9@gmail.com>
2022-07-19 21:21:27 +0800

    cleared 11th

---
+++ components/AppTodo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AppTodo = () => {
+    return (
+        <div>
+            <input type="checkbox" /> I cleared FES <br />
+            <input type="checkbox" /> I made some project clones <br />
+            <input type="checkbox" /> I studied for interviews <br />
+            <input type="checkbox" /> I apply for MANGA companies <br />
+        </div>
+    );
+}
+
+export default AppTodo;
--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Content from './components/Content';
-import Footer from "./components/Footer"
-import Apps from "./components/App"
+import AppTodo from "./components/AppTodo"
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Apps />
+    <AppTodo />
   </React.StrictMode>
 );
 
