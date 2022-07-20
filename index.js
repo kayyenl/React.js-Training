c94fa168a8d5c70899fee5085828dc87f0f70b74
kayyenl <yenyenlee9@gmail.com>
2022-07-20 14:33:47 +0800

    mastered props

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppTodo from "./components/AppTodo"
 import App from "./components/App"
+import ContactCard from './components/ContactCard';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppTodo />
+    <ContactCard 
+      name = "Mr. Whiskerson" 
+      imgUrl = "https://placekitten.com/300/200" 
+      phone = "(212) 555-1234"
+      email = "mr-whiskaz@capnap.meow"/>
+    <ContactCard 
+      name = "Fluffykins" 
+      imgUrl = "https://placekitten.com/400/400" 
+      phone = "(212) 555-2345"
+      email = "fluff@me.com"/>
+    <ContactCard 
+      name = "Destroyer" 
+      imgUrl = "https://placekitten.com/400/300" 
+      phone = "(212) 555-3456"
+      email = "ofworlds@yahoo.meow"/>
+    <ContactCard 
+      name = "Felix" 
+      imgUrl = "https://placekitten.com/400/350" 
+      phone = "(212) 555-4567"
+      email = "thecat@hotmail.meow"/>
   </React.StrictMode>
 );
 
