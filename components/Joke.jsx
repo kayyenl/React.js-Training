e504e258e844931c21224d52d8fef3324028c521
kayyenl <yenyenlee9@gmail.com>
2022-07-20 22:33:10 +0800

    at 1:43 oh god

--- components/Joke.jsx
+++ components/Joke.jsx
@@ -4,15 +4,17 @@ const Joke = (props) => {
     const styles = {
         fontWeight : "bold",
     }
-    let newstring = ""
-    if (props.contents.question != "") {
-        const holder = props.contents.question;
-        newstring = "Question: "  + holder
-    }
+    // let newstring = ""
+    // if (props.contents.question != "") {
+    //     const holder = props.contents.question;
+    //     newstring = "Question: "  + holder
+    // }
     return (
         <div className="joke">
-            <div className="question">{`${newstring}`}</div>
+            <div className="question" style = {{display: props.contents.question ? "block": "none"}}>Question: {props.contents.question}</div>
             <div className="punchline" style = {styles}>{props.contents.punchline}</div>
+            <br />
+            <hr />
         </div>
     );
 }
