455f3769986cb89ca900c27f83ecd13684e5085b
kayyenl <yenyenlee9@gmail.com>
2022-07-21 21:12:56 +0800

    at 2:07

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ import './index.css';
 import AppTodo from './components/AppTodo'
 import Products from './components/Products.jsx';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <Greeting />
+    <AppTodo />
   </React.StrictMode>
 );
+
 
 
 
@@ -36,3 +37,44 @@ root.render(
 //import React from 'react';
 //import ReactDOM from 'react-dom';
 
+
+// class Header extends React.Component {
+//   timeString() {
+//     const date = new Date()
+//     const hours = date.getHours()
+//     if (hours > 18) {
+//       return "Good night!"
+//     } return "Good morning!"
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>
+//           {this.timeString()}
+//         </h1>
+//       </div>
+//     )
+//   }
+// }
+
+// class Greeting extends React.Component {
+//   render() {
+//     return (
+//       <p>
+//         Welcome, {this.props.username}!
+//       </p>
+//     )
+//   }
+// }
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Greeting username="sir or madam"/>
+//         <Header />
+//       </div>
+//     )
+//   }
+// }
