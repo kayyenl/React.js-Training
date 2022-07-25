20df5e9029f3984f1327e08eb4a00726ee67dd2a
kayyenl <yenyenlee9@gmail.com>
2022-07-26 01:00:00 +0800

    figured how to make a quick ternary loading state

--- components/Fetch.jsx
+++ components/Fetch.jsx
@@ -3,20 +3,28 @@ import React, { Component } from 'react';
 class Fetch extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            loading: false
+        }
     }
 
     async componentDidMount() {
+        this.setState({loading: true})
         const apiCall = await fetch("https://swapi.dev/api/people/1")
-        const callJson = apiCall.json()
+        const callJson = await apiCall.json()
         console.log(callJson)
-        
+        this.setState({
+            name: callJson.name,
+            height: callJson.height,
+            loading: false
+        })
     }
 
     render() {
         return (
             <div>
-                
+                <h1>{this.state.loading ? "loading..." : this.state.name}</h1>
+                <p>height: {this.state.loading ? "loading..." : this.state.height + "cm"}</p>
             </div>
         );
     }
