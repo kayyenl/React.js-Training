613d32bcfc495b1b2ea112ed3120c72065e2cfa0
kayyenl <yenyenlee9@gmail.com>
2022-07-25 23:29:20 +0800

    trying this 3:23 exercise

--- components/TodoItem.jsx
+++ components/TodoItem.jsx
@@ -10,11 +10,24 @@ const TodoItem = (props) => {
         maxWidth: "500px",
         border: "1px solid black",
         padding: "32px 8px",
-        margin: "0 auto"
+        margin: "0 auto",
+    }
+    
+    const condStyles = {
+        textDecoration: "line-through"
+    }
+
+    const noneStyles = {
+        textDecoration: "none"
+    }
+
+    function styleDecider(bool) {
+        return bool ? {noneStyles} : {condStyles}
     }
     return (
         <div style={styles}>
-             <input  type="checkbox" onChange={() => props.handleChange(props.info.id)} checked={props.info.completed} /> {props.info.text} <br />
+             <input type="checkbox" onChange={() => props.handleChange(props.info.id)} checked={props.info.completed} /> 
+             <span style={() => styleDecider(props.info.completed)}>{props.info.text}</span> <br />
         </div>
     );
 }
