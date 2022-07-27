4573f0380da2317b3cb3b110bda9350c6a268e0f
kayyenl <yenyenlee9@gmail.com>
2022-07-28 00:30:04 +0800

    done with the forms practice

--- components/TravelForm.jsx
+++ components/TravelForm.jsx
@@ -8,15 +8,23 @@ class TravelForm extends Component {
             lastname: "",
             age: "",
             gender: "",
-            
+            location: "",
+            beef: false,
+            chicken: false,
+            plantbased: false 
         }
     }
 
     handleClick(event) {
-        const {name, value} = event.target
-        this.setState({
-            [name] : value
-        })
+        const {name, value, type, checked} = event.target
+        if (type !== "checkbox") {
+            this.setState({
+                [name] : value
+            })
+        }
+        else {
+            this.setState({[name] : checked})
+        }
         console.log(name)
         console.log(value)
     }
@@ -25,15 +33,43 @@ class TravelForm extends Component {
         return (
             <main>
                 <form>
-                    First name: <input type="text" name='firstname' value={this.state.firstname} onChange={(event) => this.handleClick(event)} /> 
+                    First name: <input type="text" placeholder='First Name' name='firstname' value={this.state.firstname} onChange={(event) => this.handleClick(event)} /> 
                     <br />
-                    Last name: <input type="text" name='lastname' value={this.state.lastname} onChange={(event) => this.handleClick(event)}/>
+                    Last name: <input type="text" placeholder='Last Name' name='lastname' value={this.state.lastname} onChange={(event) => this.handleClick(event)}/>
                     <br />
-                    Age: <input type="number" name='age' value={this.state.age} onChange={(event) => this.handleClick(event)}/>
+                    Age: <input type="number" name='age' placeholder='Age' value={this.state.age} onChange={(event) => this.handleClick(event)}/>
                     <br />
-                    Male<input type="radio" name='gender' value={"male"} checked={this.state.gender === "male"} onChange={(event) => this.handleClick(event)} />
-                    Female<input type="radio" name='gender' value={"female"} checked={this.state.gender === "female"} onChange={(event) => this.handleClick(event)}/>
+                    <label>
+                    Male<input type="radio" name='gender' value={"Male"} checked={this.state.gender === "Male"} onChange={(event) => this.handleClick(event)} />
+                    </label>
+                    <label>
+                    Female<input type="radio" name='gender' value={"Female"} checked={this.state.gender === "Female"} onChange={(event) => this.handleClick(event)}/>
+                    </label>
+                    <br />
+                    <label>Select where to go!  </label>
+                    <select name="location" value={this.state.location} onChange={(event) => this.handleClick(event)}>
+                        <option value="">--------</option>
+                        <option value="Tokyo">Tokyo</option>
+                        <option value="Kyoto">Kyoto</option>
+                        <option value="Fukuoka">Fukuoka</option>
+                    </select>
+                    <br /> Dietary plans:
+                    <br />
+                    <input type="checkbox" name="beef" checked={this.state.beef} onChange={(event) => this.handleClick(event)} /> Beef  <br />
+                    <input type="checkbox" name="chicken" checked={this.state.chicken} onChange={(event) => this.handleClick(event)} /> Chicken <br />
+                    <input type="checkbox" name="plantbased" checked={this.state.plantbased} onChange={(event) => this.handleClick(event)} /> Plant-based
                 </form>
+                <hr />
+                <h2>Entered Information</h2>
+                <p>Your name: {this.state.firstname} {this.state.lastname}</p>
+                <p>Your age: {this.state.age}</p>
+                <p>Your gender: {this.state.gender}</p>
+                <p>Your destination: {this.state.location}</p>
+                <p>Your diet plans: <br />
+                    Beef? <b>{this.state.beef ? "Yes" : "No"}</b> <br />
+                    Chicken? <b>{this.state.chicken ? "Yes" : "No"}</b> <br />
+                    Plant-Based? <b>{this.state.plantbased ? "Yes" : "No"}</b> <br />
+                </p>
             </main>
         );
     }
