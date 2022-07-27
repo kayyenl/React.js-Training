93092f1c63872b6d9b74f3fa76246e64e0f92c76
kayyenl <yenyenlee9@gmail.com>
2022-07-27 15:04:05 +0800

    learning forms and states

---
+++ components/Forms.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Forms extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName : "",
+            lastName: ""
+        }
+    }
+
+    updateInput(event) {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+        console.log(event.target.name)
+        console.log(event.target.value)
+    }
+
+    render() {
+        return (
+            <form>
+                <input type="text" placeholder='firstname' name='firstName' onChange={(event) => this.updateInput(event)}/>
+                <br />
+                <input type="text" placeholder='lastname' name='lastName' onChange={(event) => this.updateInput(event)}/>
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </form>
+        );
+    }
+}
+
+export default Forms;
