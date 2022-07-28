5ca2542553960258886c2953e2d0b0b2a0ab2094
kayyenl <yenyenlee9@gmail.com>
2022-07-28 15:56:59 +0800

    getiing tired but 4:51

--- components/MemeGenerator.jsx
+++ components/MemeGenerator.jsx
@@ -25,6 +25,8 @@ class MemeGenerator extends Component {
         this.setState({
             [name] : value
         })
+        console.log(name)
+        console.log(value)
     }
     
     
@@ -47,15 +49,19 @@ class MemeGenerator extends Component {
             transition: "all 300ms ease",
             boxShadow: "0 0 8px"
         }
-        const inputformat = {
+        const formformat = {
             marginBottom: "12px",
+            display: "flex",
             justifyContent: "center",
+            flexDirection: "column"
+        }
+        const inputformat = {
             alignItems: "center"
         }
 
         return (
             <div style = {format}>
-                <form >
+                <form style= {formformat} >
                     <input style= {inputformat} placeholder="Top Text" type="text" name='topText' value={this.state.topText} onChange={(event) => this.handleChange(event)}/> <br />
                     <input style= {inputformat} placeholder="Bottom Text" type="text" name='bottomText' value={this.state.bottomText} onChange={(event) => this.handleChange(event)}/> <br />
                     <button style = {styles}>
